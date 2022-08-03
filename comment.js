8730af7c0dd51cfacff639a3a4894fd36362a4c7
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2022-08-04 00:25:01 +0600

    last commit

--- comment.js
+++ comment.js
@@ -1,6 +1,5 @@
 document.getElementById('btn').addEventListener('click',function(){
     const commentBox = document.getElementById('new-comment');
-   // const userComment = commentBox.value;
     const newComment = document.createElement('p');
     newComment.innerText = commentBox.value;
     document.getElementById('comment-container').appendChild(newComment);
